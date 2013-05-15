f823bd7fe8033b769ac096359b012e3bd0637d06
Oren Goldberg <ogoldberg@gmail.com>
2013-05-15 17:50:51 -0400

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Karma::Application.config.secret_token = 'c4928dbc7a2f041e1ad364cb4cc4c6e457fa61c1ac31674c37cd89fe77bc809cf3fea211856e499bbe0e4ac89baf56927ce98956cf01de8b8d62fc1fcf82d80f'
